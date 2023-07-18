010bbf80b416d2d5471fe4abe38d4dbfbcff6a5c
Monika <monika.zemankiewicz.2015@gmail.com>
2023-07-18 21:58:17 +0200

    to deploy

--- src/components/AllImages.js
+++ src/components/AllImages.js
@@ -4,7 +4,7 @@ import ImagesList from "./ImagesList";
 const AllImages = () => {
   const [pictures, setPictures] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:8000/api/images/")
+    fetch("https://monikareactdjango.pythonanywhere.com/api/images/")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
